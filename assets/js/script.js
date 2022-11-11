4112c4431be24adfd1124af4de343e7f71d42f7a
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-11 05:56:11 -0800

    basic button setup & Start quiz page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var countdown = 60;
+var startCountDown = false;
 
 setInterval(function()
 {
-    if (countdown > 0)
+    if (countdown > 0 && startCountDown === true)
     {
         countdown--;
         document.getElementById("timer").innerHTML = "Timer: " + countdown;
