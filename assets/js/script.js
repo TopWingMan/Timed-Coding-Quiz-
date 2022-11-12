8ad3af0045e72f18f3b3b90633f61cc03463a8ea
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-12 08:28:07 -0800

    added 3 more questions and changed some formating of flex boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var countdown = 61;
 var startCountDown = false;
+var feedbackAnswer = "Answer!";
 
 setInterval(function()
 {
@@ -10,9 +11,46 @@ setInterval(function()
     }
 }, 1000)
 
-document.getElementById("start-button").addEventListener("click", function()
+document.getElementById("start-button").addEventListener("click", function page1(event)
 {
-    console.log("Cheese");
-    document.getElementById("starting-page").style.visibility = "hidden";
+    // event.stopPropagation()
+    document.getElementById("starting-page").style.display = "none";
+    document.getElementById("question-page-1").style.display = "flex";
     startCountDown = true;
+})
+
+document.getElementById("question-button-1").addEventListener("click", function page2(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-2").style.display = "flex";
+    document.getElementById("question-page-1").style.display = "none";
+    document.getElementById("feedback").style.display = "flex";
+})
+
+document.getElementById("question-button-2").addEventListener("click", function page3(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-3").style.display = "flex";
+    document.getElementById("question-page-2").style.display = "none";
+})
+
+document.getElementById("question-button-3").addEventListener("click", function page4(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-4").style.display = "flex";
+    document.getElementById("question-page-3").style.display = "none";
+})
+
+document.getElementById("question-button-4").addEventListener("click", function page5(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-5").style.display = "flex";
+    document.getElementById("question-page-4").style.display = "none";
+})
+
+document.getElementById("question-button-5").addEventListener("click", function page6(event)
+{
+    // event.stopPropagation()
+    //go to score page
+    document.getElementById("question-page-5").style.display = "none";
 })
