99f2df2614c86d53b3c74de96afef8d33f32638b
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-13 13:43:18 -0800

    made the other buttons work on the questions pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ setInterval(function()
     }
 }, 1000)
 
-document.getElementById("start-button").addEventListener("click", function page1(event)
+document.getElementById("start-button").addEventListener("click", function question1Button(event)
 {
     // event.stopPropagation()
     document.getElementById("starting-page").style.display = "none";
@@ -19,7 +19,29 @@ document.getElementById("start-button").addEventListener("click", function page1
     startCountDown = true;
 })
 
-document.getElementById("question-button-1").addEventListener("click", function page2(event)
+//question page 1
+document.getElementById("question-button-1-1").addEventListener("click", function page2(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-2").style.display = "flex";
+    document.getElementById("question-page-1").style.display = "none";
+    document.getElementById("feedback").style.display = "flex";
+})
+document.getElementById("question-button-1-2").addEventListener("click", function page2(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-2").style.display = "flex";
+    document.getElementById("question-page-1").style.display = "none";
+    document.getElementById("feedback").style.display = "flex";
+})
+document.getElementById("question-button-1-3").addEventListener("click", function page2(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-2").style.display = "flex";
+    document.getElementById("question-page-1").style.display = "none";
+    document.getElementById("feedback").style.display = "flex";
+})
+document.getElementById("question-button-1-4").addEventListener("click", function page2(event)
 {
     // event.stopPropagation()
     document.getElementById("question-page-2").style.display = "flex";
@@ -27,28 +49,104 @@ document.getElementById("question-button-1").addEventListener("click", function 
     document.getElementById("feedback").style.display = "flex";
 })
 
-document.getElementById("question-button-2").addEventListener("click", function page3(event)
+//question page 2
+document.getElementById("question-button-2-1").addEventListener("click", function page3(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-3").style.display = "flex";
+    document.getElementById("question-page-2").style.display = "none";
+})
+document.getElementById("question-button-2-2").addEventListener("click", function page3(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-3").style.display = "flex";
+    document.getElementById("question-page-2").style.display = "none";
+})
+document.getElementById("question-button-2-3").addEventListener("click", function page3(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-3").style.display = "flex";
+    document.getElementById("question-page-2").style.display = "none";
+})
+document.getElementById("question-button-2-4").addEventListener("click", function page3(event)
 {
     // event.stopPropagation()
     document.getElementById("question-page-3").style.display = "flex";
     document.getElementById("question-page-2").style.display = "none";
 })
 
-document.getElementById("question-button-3").addEventListener("click", function page4(event)
+//question page 3
+document.getElementById("question-button-3-1").addEventListener("click", function page4(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-4").style.display = "flex";
+    document.getElementById("question-page-3").style.display = "none";
+})
+document.getElementById("question-button-3-2").addEventListener("click", function page4(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-4").style.display = "flex";
+    document.getElementById("question-page-3").style.display = "none";
+})
+document.getElementById("question-button-3-3").addEventListener("click", function page4(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-4").style.display = "flex";
+    document.getElementById("question-page-3").style.display = "none";
+})
+document.getElementById("question-button-3-4").addEventListener("click", function page4(event)
 {
     // event.stopPropagation()
     document.getElementById("question-page-4").style.display = "flex";
     document.getElementById("question-page-3").style.display = "none";
 })
 
-document.getElementById("question-button-4").addEventListener("click", function page5(event)
+//question page 4
+document.getElementById("question-button-4-1").addEventListener("click", function page5(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-5").style.display = "flex";
+    document.getElementById("question-page-4").style.display = "none";
+})
+document.getElementById("question-button-4-2").addEventListener("click", function page5(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-5").style.display = "flex";
+    document.getElementById("question-page-4").style.display = "none";
+})
+document.getElementById("question-button-4-3").addEventListener("click", function page5(event)
+{
+    // event.stopPropagation()
+    document.getElementById("question-page-5").style.display = "flex";
+    document.getElementById("question-page-4").style.display = "none";
+})
+document.getElementById("question-button-4-4").addEventListener("click", function page5(event)
 {
     // event.stopPropagation()
     document.getElementById("question-page-5").style.display = "flex";
     document.getElementById("question-page-4").style.display = "none";
 })
 
-document.getElementById("question-button-5").addEventListener("click", function page6(event)
+//question page 5
+document.getElementById("question-button-5-1").addEventListener("click", function page6(event)
+{
+    // event.stopPropagation()
+    //go to score page
+    document.getElementById("question-page-5").style.display = "none";
+})
+document.getElementById("question-button-5-2").addEventListener("click", function page6(event)
+{
+    // event.stopPropagation()
+    //go to score page
+    document.getElementById("question-page-5").style.display = "none";
+})
+document.getElementById("question-button-5-3").addEventListener("click", function page6(event)
+{
+    // event.stopPropagation()
+    //go to score page
+    document.getElementById("question-page-5").style.display = "none";
+})
+document.getElementById("question-button-5-4").addEventListener("click", function page6(event)
 {
     // event.stopPropagation()
     //go to score page
