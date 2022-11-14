7c0acc8c883a7e999eb88b955f6d8fbffdf1b11f
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-11-13 17:58:41 -0800

    questions now give user feedback after answering

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ var answer3 = "Wrong!";
 var answer4 = "Wrong!";
 var answer5 = "Wrong!";
 
+var answerFeedbackCorrect = '<h1>_______________________ <br>Correct!</h1>';
+var answerFeedbackWrong = '<h1>_______________________ <br>Wrong!</h1>';
+
+var feedback1 = document.querySelector('#feedback-1');
+var feedback2 = document.querySelector('#feedback-2');
+var feedback3 = document.querySelector('#feedback-3');
+var feedback4 = document.querySelector('#feedback-4');
+var feedback5 = document.querySelector('#feedback-5'); //this one is not used yet but will be
+
 setInterval(function()
 {
     if (countdown > 0 && startCountDown === true)
@@ -18,7 +27,6 @@ setInterval(function()
 
 document.getElementById("start-button").addEventListener("click", function question1Button(event)
 {
-    // event.stopPropagation()
     document.getElementById("starting-page").style.display = "none";
     document.getElementById("question-page-1").style.display = "flex";
     startCountDown = true;
@@ -27,21 +35,23 @@ document.getElementById("start-button").addEventListener("click", function quest
 //question page 1
 document.getElementById("question-button-1-1").addEventListener("click", function page2(event)
 {
-    // event.stopPropagation()
+    feedback1.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-2").style.display = "flex";
     document.getElementById("question-page-1").style.display = "none";
     document.getElementById("feedback").style.display = "flex";
+    incorrectAnswer();
 })
 document.getElementById("question-button-1-2").addEventListener("click", function page2(event)
 {
-    // event.stopPropagation()
+    feedback1.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-2").style.display = "flex";
     document.getElementById("question-page-1").style.display = "none";
     document.getElementById("feedback").style.display = "flex";
+    incorrectAnswer();
 })
 document.getElementById("question-button-1-3").addEventListener("click", function page2(event) //Correct answer
 {
-    // event.stopPropagation()
+    feedback1.innerHTML = answerFeedbackCorrect;
     document.getElementById("question-page-2").style.display = "flex";
     document.getElementById("question-page-1").style.display = "none";
     document.getElementById("feedback").style.display = "flex";
@@ -49,61 +59,68 @@ document.getElementById("question-button-1-3").addEventListener("click", functio
 })
 document.getElementById("question-button-1-4").addEventListener("click", function page2(event)
 {
-    // event.stopPropagation()
+    feedback1.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-2").style.display = "flex";
     document.getElementById("question-page-1").style.display = "none";
     document.getElementById("feedback").style.display = "flex";
+    incorrectAnswer();
 })
 
 //question page 2
 document.getElementById("question-button-2-1").addEventListener("click", function page3(event)
 {
-    // event.stopPropagation()
+    feedback2.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-3").style.display = "flex";
     document.getElementById("question-page-2").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-2-2").addEventListener("click", function page3(event)
 {
-    // event.stopPropagation()
+    feedback2.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-3").style.display = "flex";
     document.getElementById("question-page-2").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-2-3").addEventListener("click", function page3(event) //correct answer
 {
-    // event.stopPropagation()
+    feedback2.innerHTML = answerFeedbackCorrect;
     document.getElementById("question-page-3").style.display = "flex";
     document.getElementById("question-page-2").style.display = "none";
     answer2 = "Correct!";
 })
 document.getElementById("question-button-2-4").addEventListener("click", function page3(event)
 {
-    // event.stopPropagation()
+    feedback2.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-3").style.display = "flex";
     document.getElementById("question-page-2").style.display = "none";
+    incorrectAnswer();
 })
 
 //question page 3
 document.getElementById("question-button-3-1").addEventListener("click", function page4(event)
 {
-    // event.stopPropagation()
+    feedback3.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-4").style.display = "flex";
     document.getElementById("question-page-3").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-3-2").addEventListener("click", function page4(event)
 {
-    // event.stopPropagation()
+    feedback3.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-4").style.display = "flex";
     document.getElementById("question-page-3").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-3-3").addEventListener("click", function page4(event)
 {
-    // event.stopPropagation()
+    feedback3.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-4").style.display = "flex";
     document.getElementById("question-page-3").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-3-4").addEventListener("click", function page4(event) //correct answer
 {
-    // event.stopPropagation()
+    feedback3.innerHTML = answerFeedbackCorrect;
     document.getElementById("question-page-4").style.display = "flex";
     document.getElementById("question-page-3").style.display = "none";
     answer3 = "Correct!";
@@ -112,55 +129,61 @@ document.getElementById("question-button-3-4").addEventListener("click", functio
 //question page 4
 document.getElementById("question-button-4-1").addEventListener("click", function page5(event)
 {
-    // event.stopPropagation()
+    feedback4.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-5").style.display = "flex";
     document.getElementById("question-page-4").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-4-2").addEventListener("click", function page5(event)
 {
-    // event.stopPropagation()
+    feedback4.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-5").style.display = "flex";
     document.getElementById("question-page-4").style.display = "none";
+    incorrectAnswer();
 })
 document.getElementById("question-button-4-3").addEventListener("click", function page5(event) //correct answer
 {
-    // event.stopPropagation()
+    feedback4.innerHTML = answerFeedbackCorrect;
     document.getElementById("question-page-5").style.display = "flex";
     document.getElementById("question-page-4").style.display = "none";
     answer4 = "Correct!";
 })
 document.getElementById("question-button-4-4").addEventListener("click", function page5(event)
 {
-    // event.stopPropagation()
+    feedback4.innerHTML = answerFeedbackWrong;
     document.getElementById("question-page-5").style.display = "flex";
     document.getElementById("question-page-4").style.display = "none";
+    incorrectAnswer();
 })
 
 //question page 5
 document.getElementById("question-button-5-1").addEventListener("click", function page6(event)
 {
-    // event.stopPropagation()
+    //feedback5.innerHTML = answerFeedbackWrong;
     //go to score page
     document.getElementById("question-page-5").style.display = "none";
+    incorrectAnswer();
     logAnswers();
 })
 document.getElementById("question-button-5-2").addEventListener("click", function page6(event)
 {
-    // event.stopPropagation()
+    //feedback5.innerHTML = answerFeedbackWrong;
     //go to score page
     document.getElementById("question-page-5").style.display = "none";
+    incorrectAnswer();
     logAnswers();
 })
 document.getElementById("question-button-5-3").addEventListener("click", function page6(event)
 {
-    // event.stopPropagation()
+    //feedback5.innerHTML = answerFeedbackWrong;
     //go to score page
     document.getElementById("question-page-5").style.display = "none";
+    incorrectAnswer();
     logAnswers();
 })
 document.getElementById("question-button-5-4").addEventListener("click", function page6(event) //correct answer
 {
-    // event.stopPropagation()
+    //feedback5.innerHTML = answerFeedbackCorrect;
     //go to score page
     document.getElementById("question-page-5").style.display = "none";
     answer5 = "Correct!";
@@ -170,4 +193,9 @@ document.getElementById("question-button-5-4").addEventListener("click", functio
 function logAnswers()
 {
     console.log(answer1, answer2, answer3, answer4, answer5)
+}
+
+function incorrectAnswer()
+{
+    countdown -= 10;
 }
